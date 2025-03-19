7d4d77f46fd29f6a0843e201bef94c65f46ae710
Jordon Bedwell <jordon@envygeeks.io>
2025-03-19 02:49:47 -0500

    feat(ops): Add Cdktf for other things

---
+++ cdktf/cdktf.ts
@@ -0,0 +1,8 @@
+import { RepoStack } from './lib/repo';
+import { App } from 'cdktf';
+
+const app = new App();
+new RepoStack(
+  app, "GithubRepo"
+);
+app.synth();
---
+++ cdktf/lib/repo.ts
@@ -0,0 +1,84 @@
+import { DataGithubUser } from '@cdktf/provider-github/lib/data-github-user';
+import { GithubProvider } from '@cdktf/provider-github/lib/provider';
+import { BranchProtection } from '@cdktf/provider-github/lib/branch-protection';
+import { Repository } from '@cdktf/provider-github/lib/repository';
+import { type App, TerraformStack } from 'cdktf';
+import { get } from 'env-var';
+
+const githubRepo = get('GITHUB_REPO').required(true).asString()
+const githubOwner = get('GITHUB_OWNER')
+  .required(true).asString()
+
+export class RepoStack extends TerraformStack {
+  constructor(scope: App, id: string) {
+    super(scope, id);
+    
+    /**
+     * Init Github
+     */
+    new GithubProvider(
+      this, "Github", {
+        token: process.env.GITHUB_TOKEN!
+      }
+    );
+    
+    /**
+     * Owner
+     */
+    const owner = new DataGithubUser(
+      this, 'RepoOwner', {
+        username: githubOwner
+      }
+    );
+    
+    /**
+     * Repo
+     */
+    const repo = new Repository(
+      this, "Repo", {
+        name: githubRepo,
+        hasProjects: true,
+        hasDownloads: false,
+        hasDiscussions: false,
+        allowAutoMerge: false,
+        allowUpdateBranch: true,
+        deleteBranchOnMerge: true,
+        allowMergeCommit: false,
+        allowRebaseMerge: false,
+        allowSquashMerge: true,
+        visibility: 'public',
+        hasIssues: true,
+        hasWiki: false,
+      }
+    );
+    
+    /**
+     * Branch Rule
+     * Main
+     */
+    new BranchProtection(this, "ProtectMain", {
+      repositoryId: repo.nodeId,
+      pattern: 'main',
+      allowsDeletions: false,
+      allowsForcePushes: false,
+      requiredLinearHistory: true,
+      requireConversationResolution: true,
+      requireSignedCommits: false,
+      enforceAdmins: true,
+      forcePushBypassers: [
+        owner.nodeId,
+      ],
+      requiredPullRequestReviews: [
+        {
+          dismissStaleReviews: true,
+          requireCodeOwnerReviews: true,
+          requireLastPushApproval: true,
+          restrictDismissals: true,
+          pullRequestBypassers: [
+            owner.nodeId,
+          ]
+        },
+      ]
+    });
+  }
+}
