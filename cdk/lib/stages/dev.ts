799d5ce23fee233cfd615dacfcb0852ec98197cb
Jordon Bedwell <jordon@envygeeks.io>
2025-03-23 00:43:56 -0500

    refactor(cdk): app should be an env-var

--- cdk/lib/stages/dev.ts
+++ cdk/lib/stages/dev.ts
@@ -3,7 +3,7 @@ import type { Construct } from 'constructs';
 
 export class Dev extends Stage {
   public readonly env: string = 'dev';
-  public readonly app: string = 'envygeeks.io';
+  public readonly app: string = get('APP_NAME').required().asString();
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
     this.createNuxtStack();
--- cdk/lib/stages/prod.ts
+++ cdk/lib/stages/prod.ts
@@ -3,7 +3,7 @@ import type { Construct } from 'constructs';
 
 export class Prod extends Stage {
   public readonly env: string = 'prod';
-  public readonly app: string = 'envygeeks.io';
+  public readonly app: string = get('APP_NAME').required().asString();
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
     this.createNuxtStack();
